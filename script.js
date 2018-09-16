33255722d69e38432b2625ec1dfc7ba210c79234
Kot <kotvnn@msn.com>
2018-09-16 14:41:36 +0800

    Tìm giá trị lớn nhất trong mảng

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let x = [-3,5,1,3,2,10];
 let value = 1;
-for(var i = 0; i < x.length; i++){
-    if(value === x[i]){
-        console.log("Value " + x[i] + " found at " + i);
+let max = x[0];
+let index = 0;
+
+function searchinArr() {
+    for(var i = 0; i < x.length; i++){
+        if(value === x[i]){
+            console.log("Value " + x[i] + " found at " + i);
+        }
     }
+}
+
+function searchBig() {
+    for (var i = 1; i < x.length; i++) {
+        if (x[i] > max) {
+            max = x[i];
+            index = i;
+        }
+    }
+    console.log("max: " + max + " at position " + index);
 }
